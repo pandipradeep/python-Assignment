fc0b3806d931efcefd0971e906ce36129de0e848
pandipradeep <100121788+pandipradeep@users.noreply.github.com>
2022-02-21 14:23:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+fruit = input("enter the fruit name : ")
+if (fruit=="apple"):
+    print("we have apples in my shop")
+elif (fruit=="orange"):
+    print("we have orange in my shop")
+elif (fruit=="pineapple"):
+    print("we have pineapples in my shop")
+elif (fruit=="grapes"):
+    print("we have grapes in my shop")
+elif (fruit=="melon"):
+    print("we have melon in my shop")
+else:
+    print("sorry we don't have that fruit")
